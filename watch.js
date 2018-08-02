9cb38fccb3a119541fdd7e8374677c73cedeaa7f
Jared Forsyth <jared@jaredforsyth.com>
2018-08-02 01:17:13 -0700

    add example thing

---
+++ watch.js
@@ -0,0 +1,32 @@
+
+const watch = require('watch')
+const fs = require('fs')
+const c = require('child_process')
+const path = require('path')
+
+const [_, __, target] = process.argv;
+if (!target) {
+  console.log('Usage: node watch.js some/directory')
+  process.exit(10)
+}
+console.log('Watching', target)
+
+const bin = path.join(path.dirname(__filename), 'lib', 'bs', 'native', 'lisp.native')
+
+watch.watchTree(target, {
+  filter: f => f.slice(-4) === '.rel',
+  interval: .2,
+}, function (f, curr, prev) {
+  if (curr) {
+    let name = f.slice(0, -3) + 'ml'
+    if (curr.nlink === 0) {
+      fs.unlink(name)
+    } else {
+      console.log('changed', f)
+      c.exec(bin + ' bin ' + f + ' > ' + name, (e, o, er) => {
+        // console.log(o)
+        // console.log(er)
+      })
+    }
+  }
+})
